c0123fcdd751f893048cae7b4be473612b1f6efc
Antonio Silva <antoniocmsilva95@gmail.com>
2020-04-22 19:08:53 -0300

    2. Display a selection list

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './../interfaces/hero';
+import { Hero } from '../../interfaces/hero';
+import { HEROES } from '../../mocks/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,12 +8,18 @@ import { Hero } from './../interfaces/hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero =  { id: 1, name: 'Windstorm'};
+  selectedHero: Hero;
+  heroes: Hero[] = HEROES;
+
   constructor() { }
 
   //Angular calls ngOnInit() shortly after creating a component
   //It's a good place to put initialization logic
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
